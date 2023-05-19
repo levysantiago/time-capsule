4cc8e664b22bc1baf925051dc390b9443542ee84
Levy Santiago <levyssantiago@gmail.com>
2023-05-19 20:53:41 -0300

    feat: list memories page

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,23 +1,12 @@
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
-import blurBg from '../src/assets/bg-blur.png'
+import { Text, TouchableOpacity, View } from 'react-native'
+
 import * as SecureStore from 'expo-secure-store'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import Stripes from '../src/assets/stripes.svg'
+
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import { useEffect } from 'react'
 import { api } from '../src/lib/api'
 import { useRouter } from 'expo-router'
-
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-
-const StyledStripes = styled(Stripes)
 
 // Endpoint
 const discovery = {
@@ -29,12 +18,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
@@ -70,18 +53,8 @@ export default function App() {
     }
   }, [response])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className=" flex-1 items-center  px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
@@ -111,8 +84,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,15 +4,24 @@ import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
 import jwt from '@fastify/jwt'
+import fastifyMultipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastfy()
 
-app.register(cors, { origin: ['http://localhost:3000/'] })
+app.register(fastifyMultipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+app.register(cors, { origin: true })
 app.register(jwt, {
   secret: 'spacetime',
 })
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
