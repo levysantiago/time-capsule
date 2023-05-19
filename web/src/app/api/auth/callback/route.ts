8d7c4fe6adb633e1e3abc99ffc3ec2b7e2e96e3b
Levy Santiago <levyssantiago@gmail.com>
2023-05-18 22:31:47 -0300

    feat: creating new memories page

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -6,11 +6,13 @@ export async function GET(request: NextRequest) {
 
   const code = searchParams.get('code')
 
+  const redirectTo = request.cookies.get('redirectTo')?.value
+
   const registerResponse = await api.post('register', { code })
 
   const { token } = registerResponse.data
 
-  const redirectURL = new URL('/', request.url)
+  const redirectURL = redirectTo ?? new URL('/', request.url)
 
   const cookieExpireInSeconds = 60 * 60 * 24 * 30
 
